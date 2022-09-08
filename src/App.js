4c93025944200bffd5583c6b0f1d339b30940d41
Alvim-JP <alvimjp12@gmail.com>
2022-09-08 16:19:17 -0300

    Req 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import logo from './logo.svg';
+import Search from './Components/Search';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={ () => <Search /> } />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
