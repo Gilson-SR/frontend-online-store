77e38942f669b2cc8ea2b5dd254fda61a981a99a
Alvim-JP <alvimjp12@gmail.com>
2022-09-08 18:06:20 -0300

    req3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Search from './Components/Search';
 import './App.css';
+import BuyCart from './Components/BuyCart';
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" render={ () => <Search /> } />
+          <Route exact path="/BuyCart" render={ () => <BuyCart /> } />
         </Switch>
       </BrowserRouter>
     );
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Search extends React.Component {
   render() {
     return (
       <div data-testid="home-initial-message">
+        <input type="text" />
         Digite algum termo de pesquisa ou escolha uma categoria.
+        <Link
+          to="/BuyCart"
+          data-testid="shopping-cart-button"
+        >
+          carrinho
+        </Link>
       </div>
     );
   }
