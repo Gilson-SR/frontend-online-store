dd0d0fa8ec27d6e8bde3ebe60d687dabe41f870a
Gilson-SR <gilsonsrodrigues87@gmail.com>
2022-09-09 15:17:39 -0300

    req 5

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
+import {
+  getCategories,
+  getProductsFromCategoryAndQuery,
+} from '../services/api';
 import Product from '../Components/Product';
 
 class Search extends React.Component {
@@ -8,7 +11,7 @@ class Search extends React.Component {
     listCategories: [],
     searching: '',
     listResults: [],
-    message: false,
+    message: true,
   };
 
   async componentDidMount() {
@@ -24,10 +27,10 @@ class Search extends React.Component {
 
   handleClick = async () => {
     const { searching } = this.state;
-    const request = await getProductsFromCategoryAndQuery(_, searching);
+    const request = await getProductsFromCategoryAndQuery(searching);
     this.setState({
       listResults: request.results,
-      message: true,
+      message: false,
     });
   };
 
@@ -57,32 +60,23 @@ class Search extends React.Component {
           value={ searching }
           onChange={ handleChange }
         />
-        <button
-          data-testid="query-button"
-          type="button"
-          onClick={ handleClick }
-        >
+        <button data-testid="query-button" type="button" onClick={ handleClick }>
           Pesquisar
         </button>
-        Digite algum termo de pesquisa ou escolha uma categoria.
-        {
-          (listResults.length < 1) ? (
-            message && <p>Nenhum produto foi encontrado</p>
-          ) : (
-            listResults.map((element) => (<Product
-              data-testid="product"
+        {message && <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>}
+        {listResults.length < 1 ? (
+          <p>Nenhum produto foi encontrado</p>
+        ) : (
+          listResults.map((element) => (
+            <Product
               name={ element.title }
               price={ element.price }
               image={ element.thumbnail }
               key={ element.id }
             />
-            ))
-          )
-        }
-        <Link
-          to="/BuyCart"
-          data-testid="shopping-cart-button"
-        >
+          ))
+        )}
+        <Link to="/BuyCart" data-testid="shopping-cart-button">
           carrinho
         </Link>
       </div>
--- src/services/api.js
+++ src/services/api.js
@@ -9,7 +9,7 @@ export async function getCategories() {
   }
 }
 
-export async function getProductsFromCategoryAndQuery(categoryId, query) {
+export async function getProductsFromCategoryAndQuery(query, categoryId) {
   try {
     const endpoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
     const request = await fetch(endpoint);
